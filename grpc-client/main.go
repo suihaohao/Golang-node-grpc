1794ca025b6cf5f987fe1771fcf6d33e64a08720
suihaohao <1255151021@qq.com>
2018-09-29 15:49:59 +0800

    添加main.go
    向node服务发出请求并打印结果

--- grpc-client/main.go
+++ grpc-client/main.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"time"
+
+	"context"
+	pb "go-frontend/proto"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewSvgServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	res, err := c.GetSvgCaptcha(ctx, &pb.GetSvgCaptchaReq{Ip:"192.168.1.5"})
+	log.Println(res, err)
+}
