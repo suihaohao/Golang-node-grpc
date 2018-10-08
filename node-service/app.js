f7cdf74c7ebeb0f1914e6c99c4421ca04d40b01b
suihaohao <1255151021@qq.com>
2018-10-08 11:52:59 +0800

    Update app.js

--- node-service/app.js
+++ node-service/app.js
@@ -20,7 +20,7 @@ const svg_proto = grpc.loadPackageDefinition(packageDefinition).kgfamily.svc.kga
 function main() {
     console.log(`Starting svgCaptcha gRPC server on port ${PORT}...`);
     const server = new grpc.Server();
-    server.addService(svg_proto.svgService.service, {GetSvgCaptcha: captcha.createCaptcha});
+    server.addService(svg_proto.svgService.service, {GetSvgCaptcha: captcha.createCaptcha, ParseSvgCaptcha: captcha.parseCaptcha});});
     server.bind('0.0.0.0:50053', grpc.ServerCredentials.createInsecure());
     server.start();
 }
